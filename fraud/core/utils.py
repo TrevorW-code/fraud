a0647af640b3c7d2a4cbfdb78c373c7bf6971185
Trevor <trevorward001@gmail.com>
2024-12-18 07:30:21 -0700

    feat(template): added new func for combining and attempting different methods

--- fraud/core/utils.py
+++ fraud/core/utils.py
@@ -1,5 +1,14 @@
 import string
+from typing import TypeVar, List
+from collections.abc import Callable
 from functools import singledispatch
+
+class PlaceholderMethodFailed(Exception):
+    pass
+
+### find placeholders ###
+
+Template = TypeVar('T') # bind to template later or use annotated
 
 @singledispatch
 def find_placeholders(data):
@@ -9,3 +18,28 @@ def find_placeholders(data):
 def process_str(data):
     formatter = string.Formatter()
     return [field_name for _, field_name, _, _ in formatter.parse(data) if field_name]
+
+def get_value_from_method(placeholder, method):
+    try:
+        res = method(placeholder)
+        return res
+    except Exception as e:
+        # raise PlaceholderMethodFailed(f'Method Failed: {e}')
+        return None
+
+def replace_placeholders(template: Template, methods: List[Callable]) -> dict: 
+    value_mapping = {}
+    placeholders = find_placeholders(template) # gets placeholders to keep as keys
+    
+    try:
+        for placeholder in placeholders: # go through each placeholder
+            for method in methods: # go through each method
+                res = get_value_from_method(placeholder, method)
+                if res:
+                    value_mapping[placeholder] = res
+                    break
+        
+    except Exception as e:
+        raise ValueError(f"Function Mapping Missing: {e}")
+
+    return value_mapping
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
 import pytest
-from fraud.core.utils import find_placeholders
+import functools
+from unittest.mock import MagicMock
+from fraud.core.utils import find_placeholders, replace_placeholders
 from fraud.utils._decorators import set_module
+from fraud.plugins.faker import placeholder_to_faker_func, default_faker_instance
 
 @set_module('fraud')
 def fake_func():
@@ -14,6 +17,21 @@ def test_find_placeholders_not_supported():
     sample = bytes([65, 66, 67])
     with pytest.raises(NotImplementedError) as e_info:
         find_placeholders(sample)
+
+def test_replace_placeholders():
+    mock_faker = MagicMock()
+    mock_faker.name.return_value = 'John Doe'
+    
+    def custom_faker(x):
+        return placeholder_to_faker_func(x, faker_instance=mock_faker)()
+    
+    def limited_fake_name(x):
+        return 'Trevor' if x == 'fake_name' else None
+
+    template = "{fake_name}, please meet {name}"
+    out = replace_placeholders(template,methods=[limited_fake_name,custom_faker])
+    print(out)
+    assert out == {"name": "John Doe","fake_name":"Trevor"}
 
 def test_module_func():
     assert fake_func.__module__ == 'fraud'
