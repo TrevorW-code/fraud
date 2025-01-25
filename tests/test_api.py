c08de2ea9738f0413734ad86db30cc7c1a79befe
Trevor <trevorward001@gmail.com>
2025-01-25 16:18:49 -0700

    feat: added template prediction with GLiNER
    - Updated `from_template` to `from_str`
    - Implemented `predict_template` using GLiNER & replacing all of the predictions w/ test
    - Implemented `replace_word_at_indexes` w/ test
    - Implemented `make_template_from_prediction` w/ test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,9 @@
-def test_from_template_module_level_basic():
+def test_from_str_module_level_basic():
     import fraud as fr
-    out = fr.from_template("Hi! Please meet my friend {name}.",10)
+    out = fr.from_str("Hi! Please meet my friend {name}.",10)
     print(out)
 
-def test_from_template_module_level_custom():
+def test_from_str_module_level_custom():
     import fraud as fr
     
     def special_company(x):
@@ -11,5 +11,5 @@ def test_from_template_module_level_custom():
             return "RANDOM_CUSTOM_COMPANY"
         return None
 
-    out = fr.from_template("{name} works at {special_company}", 10, extra_methods=[special_company])
+    out = fr.from_str("{name} works at {special_company}", 10, extra_methods=[special_company])
     print(out)
