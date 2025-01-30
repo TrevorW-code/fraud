ffb6420c1fca09cfeb815fc67c14b74e9ee366a9
Trevor <trevorward001@gmail.com>
2025-01-29 19:25:27 -0700

    feat: added print for show model downloading

--- fraud/__init__.py
+++ fraud/__init__.py
@@ -1,5 +1,7 @@
 from fraud.core.api import from_str
+print("Downloading Default Model...")
 from fraud.plugins.gliner import predict_template
+print("Download Complete!")
 
 __all__ = [
     "from_str",
